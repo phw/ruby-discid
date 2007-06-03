154ab11ef55c710ed53d09a4cc004977b2ff8726
phw <phw@b628d043-b88c-46ef-b09a-645d37a1a36e>
2007-06-03 14:12:21 +0000

    Corrected extconf.rb
    git-svn-id: svn://rubyforge.org/var/svn/rbrainz/mb-discid/trunk@68 b628d043-b88c-46ef-b09a-645d37a1a36e

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -3,7 +3,8 @@
 
 require 'mkmf'
 
-if have_library('discid', 'discid_new')
+if have_library('discid', 'discid_new') or
+   have_library('discid.dll', 'discid_new')
   # Remove -MD from compiler flags on Windows.
   $CFLAGS.sub!('-MD', '') if RUBY_PLATFORM.include? 'win32'
   create_makefile('MB_DiscID')
