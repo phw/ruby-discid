b5f25716050345997b664bb7fa5a135cd8179276
phw <phw@b628d043-b88c-46ef-b09a-645d37a1a36e>
2011-07-02 12:49:58 +0000

    Add lib path detection copied from Nokogiri - allows out-of-the-box install when your libdiscid is in /usr/local
    Patch from https://github.com/fidothe/mb-discid
    
    
    
    git-svn-id: svn://rubyforge.org/var/svn/rbrainz/mb-discid/trunk@329 b628d043-b88c-46ef-b09a-645d37a1a36e

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -3,6 +3,52 @@
 
 require 'mkmf'
 
+LIBDIR = Config::CONFIG['libdir']
+INCLUDEDIR = Config::CONFIG['includedir']
+
+$CFLAGS << " #{ENV["CFLAGS"]}"
+$LIBS << " #{ENV["LIBS"]}"
+
+# totally thieved from Nokogiri's extconf.rb
+if Config::CONFIG['target_os'] =~ /mswin32/
+  # There's no default include/lib dir on Windows. Let's just add the Ruby ones
+  # and resort on the search path specified by INCLUDE and LIB environment
+  # variables
+  HEADER_DIRS = [INCLUDEDIR]
+  LIB_DIRS = [LIBDIR]
+
+else
+  HEADER_DIRS = [
+    # First search /opt/local for macports
+    '/opt/local/include',
+
+    # Then search /usr/local for people that installed from source
+    '/usr/local/include',
+
+    # Check the ruby install locations
+    INCLUDEDIR,
+
+    # Finally fall back to /usr
+    '/usr/include',
+  ]
+
+  LIB_DIRS = [
+    # First search /opt/local for macports
+    '/opt/local/lib',
+
+    # Then search /usr/local for people that installed from source
+    '/usr/local/lib',
+
+    # Check the ruby install locations
+    LIBDIR,
+
+    # Finally fall back to /usr
+    '/usr/lib',
+  ]
+end
+
+dir_config('discid', HEADER_DIRS, LIB_DIRS)
+
 if have_library('discid', 'discid_new') or
    have_library('discid.dll', 'discid_new')
   # Remove -MD from compiler flags on Windows.
